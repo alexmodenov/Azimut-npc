f40184d1fd2fccc99c2862c9919a004c95eacbb5
Alexander Modenov <stromdesignpr@yandex.ru>
2016-10-27 01:36:50 +0300

    Изменил код News Controller

--- www/js/app.js
+++ www/js/app.js
@@ -91,171 +91,53 @@
 
 
 
-  // News Controller / Show Latest Posts
-  // This controller gets all the posts from our WordPress site and inserts them into a variable called $scope.items
-  app.controller('newsController', [ '$http', '$scope', '$rootScope', function($http, $scope, $rootScope){
-
-    $scope.yourAPI = 'http://azimut-npc.ru/api/get_recent_posts';
-    $scope.items = [];
-    $scope.totalPages = 0;
-    $scope.currentPage = 1;
-    $scope.pageNumber = 1;
+ // News Controller
+app.filter('htmlToPlaintext', function() {
+    return function(text) {
+      return String(text).replace(/<[^>]+>/gm, '');
+    }
+  }
+);
+app.controller('newsController', [ '$http', '$scope', '$rootScope', function($http, $scope, $rootScope){
+  $scope.yourAPI = 'http://azimut-npc.ru/api/get_recent_posts';
+  $scope.items = [];
+  $scope.totalPages = 0;
+  $scope.currentPage = 1;
+  $scope.pageNumber = 1;
+  $scope.isFetching = true;
+  $scope.getAllRecords = function(pageNumber){
     $scope.isFetching = true;
-    $scope.lastSavedPage = 0;
-
-    // Let's initiate this on the first Controller that will be executed.
-    ons.ready(function() {
-      
-      // Cache Images Setup
-      // Set the debug to false before deploying your app
-      ImgCache.options.debug = true;
-
-      ImgCache.init(function(){
-
-        //console.log('ImgCache init: success!');
-        $rootScope.$broadcast('ImgCacheReady');
-        // from within this function you're now able to call other ImgCache methods
-        // or you can wait for the ImgCacheReady event
-
-      }, function(){
-        //console.log('ImgCache init: error! Check the log for errors');
-      });
-
-    });
-
-
-    $scope.pullContent = function(){
-      
-      $http.jsonp($scope.yourAPI+'/?page='+$scope.pageNumber+'&callback=JSON_CALLBACK').success(function(response) {
-
-        if($scope.pageNumber > response.pages){
-
-          // hide the more news button
-          $('#moreButton').fadeOut('fast');  
-
-        } else {
-
-          $scope.items = $scope.items.concat(response.posts);
-          window.localStorage.setObject('rootsPosts', $scope.items); // we save the posts in localStorage
-          window.localStorage.setItem('rootsDate', new Date());
-          window.localStorage.setItem("rootsLastPage", $scope.currentPage);
-          window.localStorage.setItem("rootsTotalPages", response.pages);
-
-          // For dev purposes you can remove the comment for the line below to check on the console the size of your JSON in local Storage
-          // for(var x in localStorage)console.log(x+"="+((localStorage[x].length * 2)/1024/1024).toFixed(2)+" MB");
-
-          $scope.totalPages = response.pages;
-          $scope.isFetching = false;
-
-          if($scope.pageNumber == response.pages){
-
-            // hide the more news button
-            $('#moreButton').fadeOut('fast'); 
-
-          }
-
-        }
-
-      });
-
-    }
-
-    $scope.getAllRecords = function(pageNumber){
-
-      $scope.isFetching = true;    
-
-      if (window.localStorage.getItem("rootsLastPage") == null ) {
-
-        $scope.pullContent();
-
-      } else {
-        
-        var now = new Date();
-        var saved = new Date(window.localStorage.getItem("rootsDate"));
-
-        var difference = Math.abs( now.getTime() - saved.getTime() ) / 3600000;
-
-        // Lets compare the current dateTime with the one we saved when we got the posts.
-        // If the difference between the dates is more than 24 hours I think is time to get fresh content
-        // You can change the 24 to something shorter or longer
-
-        if(difference > 24){
-          // Let's reset everything and get new content from the site.
-          $scope.currentPage = 1;
-          $scope.pageNumber = 1;
-          $scope.lastSavedPage = 0;
-          window.localStorage.removeItem("rootsLastPage");
-          window.localStorage.removeItem("rootsPosts");
-          window.localStorage.removeItem("rootsTotalPages");
-          window.localStorage.removeItem("rootsDate");
-
-          $scope.pullContent();
-        
-        } else {
-          
-          $scope.lastSavedPage = window.localStorage.getItem("rootsLastPage");
-
-          // If the page we want is greater than the last saved page, we need to pull content from the web
-          if($scope.currentPage > $scope.lastSavedPage){
-
-            $scope.pullContent();
-          
-          // else if the page we want is lower than the last saved page, we have it on local Storage, so just show it.
-          } else {
-
-            $scope.items = window.localStorage.getObject('rootsPosts');
-            $scope.currentPage = $scope.lastSavedPage;
-            $scope.totalPages = window.localStorage.getItem("rootsTotalPages");
-            $scope.isFetching = false;
-
-          }
-
-        }
-
+        $http.jsonp($scope.yourAPI+'/?page='+$scope.pageNumber+'&callback=JSON_CALLBACK').success(function(response) {
+      $scope.items = $scope.items.concat(response.posts);
+      $scope.totalPages = response.pages;
+      $scope.isFetching = false;
+      if($scope.currentPage==$scope.totalPages){
+        $('.news-page #moreButton').fadeOut('fast');  
       }
-
-    };
-
-    $scope.imgLoadedEvents = {
-        done: function(instance) {
-            angular.element(instance.elements[0]).removeClass('is-loading').addClass('is-loaded');
-        }
-    };
-
-    $scope.showPost = function(index){
-        
-      $rootScope.postContent = $scope.items[index];
+      });
+   
+  };
+  $scope.showPost = function(index){
+      
+    $rootScope.postContent = $scope.items[index];
       $scope.ons.navigator.pushPage('post.html');
-
-    };
-
-    $scope.nextPage = function(){
-
-      $scope.currentPage++; 
-      $scope.pageNumber = $scope.currentPage;                 
-      $scope.getAllRecords($scope.pageNumber);        
-
-    }
-
-  }]);
-
-  // This controller let us print the Post Content in the post.html template
-  app.controller('postController', [ '$scope', '$rootScope', '$sce', function($scope, $rootScope, $sce){
+  };
+  $scope.nextPage = function(){
     
-    $scope.item = $rootScope.postContent;
-
-    $scope.renderHtml = function (htmlCode) {
-      return $sce.trustAsHtml(htmlCode);
+    $scope.pageNumber = ($scope.currentPage + 1);
+    if($scope.pageNumber &lt;= $scope.totalPages){
+      $scope.getAllRecords($scope.pageNumber);
+      $scope.currentPage++;
+    }
+  }
+}]);
+app.controller('postController', [ '$scope', '$rootScope', '$sce', function($scope, $rootScope, $sce){
+  
+  $scope.item = $rootScope.postContent;
+  $scope.renderHtml = function (htmlCode) {
+        return $sce.trustAsHtml(htmlCode);
     };
-
-    $scope.imgLoadedEvents = {
-        done: function(instance) {
-            angular.element(instance.elements[0]).removeClass('is-loading').addClass('is-loaded');
-        }
-    };    
-
-  }]);
-
+}]);
   // Map Markers Controller
 
   app.controller('markersController', function($scope, $compile){
