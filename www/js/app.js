572bdabdcb6cabd94f769839f2bc20207c5d58be
Alexander Modenov <stromdesignpr@yandex.ru>
2017-04-03 00:49:50 +0400

    new

--- www/js/app.js
+++ www/js/app.js
@@ -95,7 +95,7 @@
   // This controller gets all the posts from our WordPress site and inserts them into a variable called $scope.items
   app.controller('newsController', [ '$http', '$scope', '$rootScope', function($http, $scope, $rootScope){
 
-    $scope.yourAPI = 'http://azimut-npc.ru/wp-json/posts';
+    $scope.yourAPI = 'http://app.azimut-npc.ru/api/get_recent_posts';
     $scope.items = [];
     $scope.totalPages = 0;
     $scope.currentPage = 1;
